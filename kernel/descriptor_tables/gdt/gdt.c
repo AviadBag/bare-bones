8091b72d21b546b385a3999485a07189f684c8d6
Aviad <aviad.bagno@gmail.com>
2020-11-12 15:55:36 +0200

    Improved gdt.c code

--- kernel/descriptor_tables/gdt/gdt.c
+++ kernel/descriptor_tables/gdt/gdt.c
@@ -5,7 +5,9 @@
 extern void gdt_flush(uint32_t);
 
 // Internal function prototypes.
-static void gdt_set_entry(int32_t, uint32_t, uint32_t, uint8_t, uint8_t);
+static void set_gdt_entry(int32_t, uint32_t, uint32_t, uint8_t, uint8_t);
+static void init_gdt_pointer();
+static void init_gdt_entries();
 
 gdt_entry_t gdt_entries[5];
 gdt_ptr_t   gdt_ptr;
@@ -14,24 +16,38 @@ void init_gdt()
 {
    vga_write_string("Initializing GDT...\n");
 
-   gdt_ptr.size = (sizeof(gdt_entry_t) * 5) - 1; // The size of the table minus 1.
-   gdt_ptr.base  = (uint32_t) & gdt_entries;
-
-   gdt_set_entry(0, 0, 0, 0, 0);                // Null segment
-   gdt_set_entry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
-   gdt_set_entry(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
-   gdt_set_entry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment
-   gdt_set_entry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
-
+   init_gdt_pointer();
+   init_gdt_entries();
    gdt_flush((uint32_t) & gdt_ptr);
 
    vga_write_string("GDT was initialized successfully!\n");
 }
 
 /**
+ * This method inits the gdt pointer
+ */
+static void init_gdt_pointer()
+{
+   gdt_ptr.size = (sizeof(gdt_entry_t) * 5) - 1; // The size of the table minus 1.
+   gdt_ptr.base  = (uint32_t) & gdt_entries;
+}
+
+/**
+ * This method inserts all the gdt entries into the table
+ */
+static void init_gdt_entries()
+{
+   set_gdt_entry(0, 0, 0, 0, 0);                // Null segment
+   set_gdt_entry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
+   set_gdt_entry(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
+   set_gdt_entry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment
+   set_gdt_entry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
+}
+
+/**
  * This method adds a new gdt entry to the table
  */
-static void gdt_set_entry(int32_t index, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
+static void set_gdt_entry(int32_t index, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
 {
    gdt_entries[index].base_low    = (base & 0xFFFF);
    gdt_entries[index].base_middle = (base >> 16) & 0xFF;
